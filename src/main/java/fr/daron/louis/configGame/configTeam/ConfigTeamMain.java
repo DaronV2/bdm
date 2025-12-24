d0cca470d42cd0ee8f89835e377b83fa655f184c
DaronV2 <darkenciel31@gmail.com>
2025-12-24 14:11:35 +0100

    Add banners to team selector and lore with team players

--- src/main/java/fr/daron/louis/configGame/configTeam/ConfigTeamMain.java
+++ src/main/java/fr/daron/louis/configGame/configTeam/ConfigTeamMain.java
@@ -2,39 +2,66 @@ package fr.daron.louis.configGame.configTeam;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
+import org.bukkit.scoreboard.Team;
+
+import fr.daron.louis.TeamUtils;
 
 import fr.daron.louis.Plugin;
 
 public class ConfigTeamMain {
-   
-   public static void openTeamMenu(Player player, JavaPlugin jvplugin){
+
+   public static void openTeamMenu(Player player, JavaPlugin jvplugin) {
       int nbTeams = ((Plugin) jvplugin).getNbTeams();
-      Inventory inv = Bukkit.createInventory(null, 9, "$0 Choissisez votre équipe");
+      Inventory inv = Bukkit.createInventory(null, 9, "§0 Choissisez votre équipe");
 
-      List<Material> banners = new ArrayList<Material>();
-      banners.add(Material.BLUE_BANNER);
-      banners.add(Material.RED_BANNER);
-      banners.add(Material.GREEN_BANNER);
-      banners.add(Material.ORANGE_BANNER);
-      banners.add(Material.PURPLE_BANNER);
-
-      for (int i = 0; i < nbTeams ; i++){
-         ItemStack banner = new ItemStack(banners.get(i));
-         ItemMeta meta = banner.getItemMeta();
-         meta.setDisplayName("Équipe "+(i+1));
-         banner.setItemMeta(meta);
-         inv.setItem(i, banner);
+      ScoreboardManager manager = Bukkit.getScoreboardManager();
+      Scoreboard board = manager.getMainScoreboard();
+      Set<Team> teams = board.getTeams();
+      int teamNumber = 0;
+      for (Team t : teams) {
+         if (teamNumber < nbTeams) {
+            Material bannerColor = TeamUtils.ChatColorToBanner(t.getColor());
+            if (bannerColor != Material.WHITE_BANNER) {
+               ItemStack banner = new ItemStack(bannerColor);
+               ItemMeta meta = banner.getItemMeta();
+               meta.setDisplayName("Équipe " + (teamNumber + 1));
+               List<String> lore = new ArrayList<>();
+               lore.add("Joueurs : ");
+               if (t.getEntries().isEmpty()) {
+                  lore.add("§c- Aucun joueur");
+               } else {
+                  for (String entry : t.getEntries()) {
+                     lore.add("§f- " + entry);
+                  }
+               }
+               meta.setLore(lore);
+               NamespacedKey key = new NamespacedKey(jvplugin, "hidden_value");
+               meta.getPersistentDataContainer().set(key, PersistentDataType.STRING, t.getName());
+               banner.setItemMeta(meta);
+               inv.addItem(banner);
+               teamNumber++;
+            }
+         }
       }
       player.openInventory(inv);
+
    }
 
 }
